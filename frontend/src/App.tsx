54dabd0979a8297d9385727ed8b7c99a7ade8887
deomiarn <deomicarnel@hotmail.com>
2025-08-06 12:28:04 +0200

    feat: create complete frontend structure and all important dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles/globals.css";
+import { Button } from "@/components/ui/button.tsx";
+
+export function App() {
+  return (
+    <div>
+      <Button>Hello World</Button>
+    </div>
+  );
+}
